c8cd46fe3aa66a2a47959bb5e19be43535a12022
Fumi Hin <fumihin@outlook.com>
2021-03-13 01:17:11 +0300

    fix: 🐛 Fixed width and height of 'MainCategoryList' component

--- client/src/components/Main-category-list/Main-category-list.tsx
+++ client/src/components/Main-category-list/Main-category-list.tsx
@@ -8,11 +8,9 @@ const AntSider = AntLayout.Sider;
 const MainCategoryList: React.FC = () => {
   return (
     <AntSider className='site-layout-background' width={200}>
-      <AntMenu style={{ width: 256 }} defaultSelectedKeys={['1']}>
+      <AntMenu mode='inline' defaultSelectedKeys={['1']} style={{ height: '100%' }}>
         <AntMenu.Item key='1'>Электроника</AntMenu.Item>
-
         <AntMenu.Item key='2'>Продукты и напитки</AntMenu.Item>
-
         <AntMenu.Item key='3'>Одежда и обувь</AntMenu.Item>
       </AntMenu>
     </AntSider>
