8d2a425187abe8d8718a9b89a11e7aa6f28e73dc
Fumi Hin <fumihin@outlook.com>
2021-03-12 23:31:40 +0300

    feat: 🎸 Added 'Footer' component

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout as AntLayout } from 'antd';
+
+import './Footer.scss';
+
+const AntFooter = AntLayout.Footer;
+
+const Footer: React.FC = () => {
+  return <AntFooter></AntFooter>;
+};
+
+export default Footer;
--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -3,13 +3,16 @@ import { Layout as AntLayout } from 'antd';
 
 import Header from '@/components/Header';
 import Content from '@/components/Content';
+import Footer from '@/components/Footer';
 
 import './Home.scss';
+
 const Home: React.FC = () => {
   return (
     <AntLayout>
       <Header />
       <Content />
+      <Footer />
     </AntLayout>
   );
 };
