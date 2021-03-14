b0c11df4a015bcba29719e4800892e20589c6742
Fumi Hin <fumihin@outlook.com>
2021-03-14 19:27:55 +0300

    feat: 🎸 Subcategory component is acting according to Body view

--- client/src/components/SubCategoryList/SubCategoryList.tsx
+++ client/src/components/SubCategoryList/SubCategoryList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout as AntLayout } from 'antd';
 import { View } from '@components/Body';
 
@@ -11,7 +11,24 @@ interface Props {
 }
 
 const SubCategoryList: React.FC<Props> = ({ view }: Props) => {
-  return <AntContent style={{ padding: '0 24px' }}>Sub categories</AntContent>;
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+
+  useEffect(() => {
+    switch (view) {
+      case View.MainCategory:
+        setIsCollapsed(false);
+        break;
+      default:
+        setIsCollapsed(true);
+        break;
+    }
+  }, [view]);
+
+  return (
+    <AntContent style={{ padding: '0 24px', display: isCollapsed ? 'none' : 'block' }}>
+      Sub categories
+    </AntContent>
+  );
 };
 
 export default SubCategoryList;
