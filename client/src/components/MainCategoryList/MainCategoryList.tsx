28a85197fd1276b1124f9ccd5fc268c571c0a151
Fumi Hin <fumihin@outlook.com>
2021-03-14 19:34:37 +0300

    feat: 🎸 Main categories is acting according to Body view

--- client/src/components/MainCategoryList/MainCategoryList.tsx
+++ client/src/components/MainCategoryList/MainCategoryList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout as AntLayout, Menu as AntMenu } from 'antd';
 import { View } from '@components/Body';
 
@@ -12,8 +12,28 @@ interface Props {
 }
 
 const MainCategoryList: React.FC<Props> = ({ view }: Props) => {
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+
+  useEffect(() => {
+    switch (view) {
+      case View.Product:
+        setIsCollapsed(true);
+        break;
+      default:
+        setIsCollapsed(false);
+        break;
+    }
+  }, [view]);
+
   return (
-    <AntSider className='site-layout-background' width='fit-content'>
+    <AntSider
+      className='site-layout-background'
+      width='fit-content'
+      style={{
+        display: isCollapsed ? 'none' : 'flex',
+        flexDirection: 'column'
+      }}
+    >
       <AntMenu mode='inline' defaultSelectedKeys={['1']} style={{ height: '100%' }}>
         {Object.keys(categoryList).map((category, key) => {
           return <AntMenu.Item key={key}>{category}</AntMenu.Item>;
